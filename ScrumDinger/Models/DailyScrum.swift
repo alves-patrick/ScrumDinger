6dae57df47532c6b5273e2dfdff9402659a11628
Patrick Alves <patrickalvesarts@gmail.com>
2024-06-04 17:10:22 -0300

    Colors

---
+++ ScrumDinger/Models/DailyScrum.swift
@@ -0,0 +1,8 @@
+//
+//  DailyScrum.swift
+//  ScrumDinger
+//
+//  Created by Patrick Alves on 04/06/24.
+//
+
+import Foundation
---
+++ ScrumDinger/Models/Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Theme.swift
+//  ScrumDinger
+//
+//  Created by Patrick Alves on 04/06/24.
+//
+
+import Foundation
