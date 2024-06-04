bb795651208b0ff32f29440ef8cf7fa2d7499321
Patrick Alves <patrickalvesarts@gmail.com>
2024-06-04 15:28:05 -0300

    Initial Commit

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Patrick Alves on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
